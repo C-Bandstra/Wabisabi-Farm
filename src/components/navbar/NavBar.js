1bbb5f7150865a2b909b1130a47e8bfa9868e12b
C-Bandstra <bandstracharlie@gmail.com>
2021-05-14 16:26:23 -0600

    Generate links based on location

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,9 +1,51 @@
 import './NavBar.css';
+import utilities from '../../utilities'
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
-function NavBar() {
+const NavBar = () => {
+  const history = useHistory();
+
+  const generateRoutes = () => {
+    let anchorLinks;
+    if(history.location.pathname !== '/') {
+      anchorLinks = (
+        <>
+          <Link onClick={() => utilities.scroll("Info")} className="link" to="/">Info</Link>
+          <Link onClick={() => utilities.scroll("Contact")}className="link" to="/">Contact</Link>
+        </>
+      )
+    } else {
+      anchorLinks = (
+        <>
+          <AnchorLink className="link" href="#Info" offset="-40">Info</AnchorLink>
+          <AnchorLink className="link" href="#Contact">Contact</AnchorLink>
+        </>
+      )
+    }
+    
+    return (
+      <>
+        <Link className="link" to="/">Home</Link>
+        <Link className="link" to="/Availability">Food Availability</Link>
+        { anchorLinks }
+        <Link className="link" to="/FAQs">FAQs</Link>
+        <span className="vertical-line"></span>
+        <Link className="link" to="/Join">Join Us</Link>
+      </>
+    )
+  }
+
+  const routes = generateRoutes()
+
+
   return (
-    <div className="navbar">
-      
+      <div className="header">
+        <img className="logo" src="/assets/logo.png" />
+        <div className="nav">
+           {routes}
+        </div>
     </div>
   );
 }
