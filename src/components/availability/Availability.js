2171f9119608cd6e36d3c8d1b34d3a5d95ab0b00
C-Bandstra <bandstracharlie@gmail.com>
2021-05-19 11:52:32 -0600

    Create Availability component

--- src/components/availability/Availability.js
+++ src/components/availability/Availability.js
@@ -1,10 +1,37 @@
 import './Availability.css';
+import ProduceCard from "../produceCard/ProduceCard"
 import NavBar from "../navbar/NavBar"
 
 const Availability = () => {
   return (
     <div className="availability">
-      <NavBar />
+      {/* <NavBar type="nav-2" title=""/> */}
+      
+      <div className="graph">
+
+        <section className="legend">
+          <p className="label">Produce</p>
+          <p className="label">JAN</p>
+          <p className="label">FEB</p>
+          <p className="label">MAR</p>
+          <p className="label">APR</p>
+          <p className="label">MAY</p>
+          <p className="label">JUNE</p>
+          <p className="label">JULY</p>
+          <p className="label">AUG</p>
+          <p className="label">SEPT</p>
+          <p className="label">OCT</p>
+          <p className="label">NOV</p>
+          <p className="label">DEC</p>
+        </section>
+
+        <ProduceCard type="arugula" special={<div className="arugula-2 time-bar"></div>}/>
+        <ProduceCard />
+
+        
+        
+      </div>
+
     </div>
   );
 }
