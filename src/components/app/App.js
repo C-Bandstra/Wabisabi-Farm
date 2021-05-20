d0c26b97801ae4bcda4dd6f8b0ef6a1b3e575274
C-Bandstra <bandstracharlie@gmail.com>
2021-05-20 12:31:38 -0600

    Render Faq Footers

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,6 +5,8 @@ import { Switch, Route } from "react-router-dom";
 import Availability from '../availability/Availability'
 import FAQ from '../faq/FAQ'
 import CTA from "../cta/CTA"
+import FooterNav from "../footerNav/FooterNav"
+import Footer from "../footer/Footer"
 import NavBar from "../navbar/NavBar"
 import Practices from "../practices/Practices"
 
@@ -19,13 +21,19 @@ const App = () => {
         <Route path="/FAQS">
           <NavBar type="nav-2" title={<p className="title">FAQ</p>}/>
           <FAQ />
-          <CTA />
+          <CTA type="water" image="/assets/water.png" header="GROWING PRACTICES" text="Learn more about how we grow our vegetables and what we put into the soil."/>
+          <FooterNav />
+          <Footer />
         </Route>
         <Route path="/Join">
           
         </Route>
         <Route path="/GrowingPractices">
+          <NavBar type="nav-2" title={<p className="title practice-title">Growing Practices</p>}/>
           <Practices />
+          <CTA type="plant" image="/assets/plant.png" header="FAQ's" text="Have questions about how the food share works, or what we feed our plants?"/>
+          <FooterNav />
+          <Footer />
         </Route>
         <Route path="/">
           <Home />
