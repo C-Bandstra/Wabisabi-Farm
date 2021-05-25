2f22c75bd42805d27d1ab01d8bfb0a521f81c6b0
C-Bandstra <bandstracharlie@gmail.com>
2021-05-25 15:50:44 -0600

    Revert anchor back to React Link and pass in route

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -1,17 +1,17 @@
 import './CTA.css';
 import { Link } from 'react-router-dom'
 
-const CTA = ({ type, image, header, text}) => {
+const CTA = ({ type, image, header, text, route}) => {
   return (
     <section className="cta">
       <div className="cta-icon-container">
         <img alt="Growth/Care" className={`${type}-icon`} src={ image }/>
       </div>
-      <a className="cta-content" href="/GrowingPractices">
+      <Link className="cta-content" to={route}>
           <h3 className="cta-header">{ header }</h3>
           <p className="cta-text">{ text }</p>
           <img alt="Arrow pointing new page" className="arrow" src="/assets/arrow1.png" />
-      </a>
+      </Link>
     </section>
   );
 }
