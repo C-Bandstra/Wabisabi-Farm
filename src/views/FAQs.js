d7792824174c33027bdce674abf4a560b7d257a4
C-Bandstra <bandstracharlie@gmail.com>
2021-06-07 11:00:37 -0600

    Update prop names

--- src/views/FAQs.js
+++ src/views/FAQs.js
@@ -9,7 +9,7 @@ const FAQs = () => {
     <section className="FAQs">
       <NavBar type="nav-2" title={<p className="title">FAQ</p>}/>
       <FAQ />
-      <CTA type="water" image="/assets/water.png" header="GROWING PRACTICES" text="Learn more about how we grow our vegetables and what we put into the soil." route="/GrowingPractices"/>
+      <CTA imgType="water" image="/assets/water.png" header="GROWING PRACTICES" text="Learn more about how we grow our vegetables and what we put into the soil." route="/GrowingPractices" arrow="1"/>
       <FooterNav />
       <Footer />
     </section>
