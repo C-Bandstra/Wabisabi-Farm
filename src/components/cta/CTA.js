3b4497cafa503645eab515a2d432bfd66faed246
C-Bandstra <bandstracharlie@gmail.com>
2021-06-07 10:57:55 -0600

    Add interpolation to class lists

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -1,16 +1,16 @@
 import './CTA.css';
 import { Link } from 'react-router-dom'
 
-const CTA = ({ type, image, header, text, route}) => {
+const CTA = ({ imgType, image, header, text, route, ctaType, color, arrow}) => {
   return (
-    <section className="cta">
+    <section className={`cta ${ctaType}`}>
       <div className="cta-icon-container">
-        <img alt="Growth/Care" className={`${type}-icon`} src={ image }/>
+        <img alt="Growth/Care" className={`${imgType} icon`} src={ image }/>
       </div>
       <Link className="cta-content" to={route}>
-          <h3 className="cta-header">{ header }</h3>
-          <p className="cta-text">{ text }</p>
-          <img alt="Arrow pointing new page" className="arrow" src="/assets/arrow1.png" />
+          <h3 className={`cta-header ${color}`}>{ header }</h3>
+          <p className={`cta-text ${color}`}>{ text }</p>
+          <img alt="Arrow pointing new page" className="arrow" src={`/assets/arrow${arrow}.png`} />
       </Link>
     </section>
   );
