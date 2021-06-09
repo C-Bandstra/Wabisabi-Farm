59d299a04683370ae57f58d37e321875be916f1a
C-Bandstra <bandstracharlie@gmail.com>
2021-06-09 13:19:02 -0600

    Add conditional Nav rendering

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,35 @@
 import './Header.css';
+import Burger from '../burger/Burger'
+import { useState, useEffect } from 'react'
 import Nav from '../nav/Nav'
 
 const Header = ({ type, title }) => {
+  const [isMobile, setMobile] = useState(window.innerWidth <= 688);
+
+  const updateView = () => {
+    setMobile(window.innerWidth <= 688);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateView);
+    return () => window.removeEventListener("resize", updateView);
+  });
 
   return (
     <div className={`header ${type}`}>
-      <img alt="Wabi Sabi Farm logo" className={`logo ${type}`} src="/assets/logo.png" />
-      <Nav type="nav-2"/>
-      <p className="title">{ title }</p>
+      { isMobile ? (
+        <>
+          <img alt="Wabi Sabi Farm logo" className={`logo ${type}`} src="/assets/logo.png" />
+          <Burger />
+          <p className="title">{ title }</p>
+        </>
+      ) : (
+        <>
+          <img alt="Wabi Sabi Farm logo" className={`logo ${type}`} src="/assets/logo.png" />
+          <Nav type="nav-2"/>
+          <p className="title">{ title }</p>
+        </>
+      )}
     </div>
   );
 }
