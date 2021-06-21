b4c1464713563d7332b5b0098bbd32706aa6662f
C-Bandstra <bandstracharlie@gmail.com>
2021-06-21 13:42:29 -0600

    Update Practices for home view rendering

--- src/components/practices/Practices.js
+++ src/components/practices/Practices.js
@@ -2,10 +2,16 @@ import './Practices.css';
 
 const Practices = () => {
   return (
-    <div className="practices">
-      <p className="practices-header">How we care for your food</p>
-      <p className="practices-content">We are a Certified Organic Fruit, Vegetable, Herb and Transplant Farm located about 20 miles NW of Des Moines in Granger, Iowa.  2021 will be our 9th year growing the high quality, nutrient dense, USDA Certified Organic AND Local produce and plants to Central Iowa!  One heated greenhouse and 2 HIGH tunnels help us prolong the season and provide food and garden plants to our community through Customizable Farm Shares, the Des Moines Farmers' Market, Restaurants and an On Farm Stand.</p>
-      <p className="practices-content">Our mission is to grow tasty, nutritious, Certified Organic (IDALS) fruits, vegetables, herbs and garden transplants available to all members of the community in the most environmentally, physically and economically sustainable way.</p>
+    <div id="Practices" className="practices">
+      <section className="practices-header-container">
+        <p className="practices-header-1">GROWING PRACTICES</p>
+        <p className="practices-header-2">How We Care For Your Food</p>
+      </section>
+      <section className="practices-text-container">
+        <p className="practices-text">Wabi Sabi Farm is USDA Certified Organic by the Iowa Department of Agriculture and Land Stewardship (IDALS).  No genetically modified crops are grown and no synthetic chemicals are sprayed.  We plant cover crops on fallow ground for soil health, but will also amend this with compost on crop ground.</p>
+        <p className="practices-text">We are fortunate to be able to irrigate from a deep well to supplement Nature's rains.  To conserve water we use trickle irrigation to focus the water on the crop rows and not between rows.  In dry years we are able to irrigate to keep the plants alive, but it is Nature's rain that helps crops produce at their best.</p>
+        <p className="practices-text">While being USDA Certified Organic by a third party certifier (IDALS), we also strive to grow with a cooperative attitude toward Nature and encourage wildlife on the farm.  We place an emphasis on using more physical controls (bird-netting, re-usable weed fabric, row covers, Surround WSP, etc.) than organically approved herbicides and pesticides (although we do use the pesticides sparingly if needed) as a way to increase our beneficial insect populations and native ecosystems.</p>
+      </section>
     </div>
   );
 }
