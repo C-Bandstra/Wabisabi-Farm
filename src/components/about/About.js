d0e7033f3d1630034ebfe53eb3896f141861ab8e
C-Bandstra <bandstracharlie@gmail.com>
2021-06-21 13:38:08 -0600

    Adjust wording

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,6 @@
 import './About.css';
-
 const About = () => {
+
   return (
     <div id="About" className="about">
       <section className="header-container">
@@ -12,12 +12,11 @@ const About = () => {
           Herb and Transplant Farm located about 20 miles
           NW of Des Moines in Granger, Iowa.  2021 will be
           our 9th year growing the high quality, nutrient
-          dense, USDA Certified Organic AND Local produce
-          and plants to Central Iowa!  One heated greenhouse
-          and 2 HIGH tunnels help us prolong the season and
-          provide food and garden plants to our community
+          dense, USDA Certified Organic AND Local produce/plants to Central Iowa!  One heated greenhouse
+          and 2 High tunnels help us prolong the season and
+          provide food/garden plants to our community
           through Customizable Farm Shares, the Des Moines
-          Farmers' Market, Restaurants and an On Farm Stand.
+          Farmers' Market and Restaurants.
         </p>
         <p className="about-text">Our mission is to grow tasty, nutritious, Certified
           Organic (IDALS) fruits, vegetables, herbs and garden
