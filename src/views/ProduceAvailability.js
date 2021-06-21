c1e5c4a37db862775437cf3d3b7d46ce69675f11
C-Bandstra <bandstracharlie@gmail.com>
2021-06-21 13:44:31 -0600

    Update cta props

--- src/views/ProduceAvailability.js
+++ src/views/ProduceAvailability.js
@@ -9,7 +9,7 @@ const ProduceAvailability = () => {
     <section className="produce-availability">
       <Header type="header-2" title="Produce Availability"/>
       <Availability />
-      <CTA imgType="water" image="/assets/water.png" header="GROWING PRACTICES" text="Learn more about how we grow our vegetables and what we put into the soil." route="/GrowingPractices" arrow="1"/>
+      <CTA imgType="plant" cntType="plant-cnt" image="/assets/plant.png" header="FAQ's" text="Have questions about how the food share works, or what we choose to feed our plants?" route="/FAQs" ctaType="plant-cta" color="light" arrow="reverse"/>
       <FooterNav />
       <Footer />
     </section>
