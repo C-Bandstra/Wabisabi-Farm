bed15f3fef12fb1e5fd50cf71aaff54d5fc69b79
C-Bandstra <bandstracharlie@gmail.com>
2021-06-21 14:03:09 -0600

    Update produce availability header

--- src/components/availability/Availability.js
+++ src/components/availability/Availability.js
@@ -4,7 +4,7 @@ const Availability = () => {
   return (
     <div className="availability">
       <p className="availability-header-1">WHAT'S IN SEASON</p>
-      <p className="availability-header-2">Wabi Sabi Growth Graph</p>
+      <p className="availability-header-2">Wabi Sabi Produce Map</p>
       <div className="graph-container" >
         <img className="availability-legend" src="/assets/legend.png" />
         <img className="availability-graph" src="/assets/graph-dark.png" />
