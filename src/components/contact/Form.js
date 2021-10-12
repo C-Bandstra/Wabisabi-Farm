9617d40fcd7fe2df517f20b7dd687d87d27b793b
C-Bandstra <bandstracharlie@gmail.com>
2021-10-12 16:18:20 -0600

    Add emailJS functionality

--- src/components/contact/Form.js
+++ src/components/contact/Form.js
@@ -1,7 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import emailjs, { init } from 'emailjs-com'
 
 const Form = () => {
-  const [status, setStatus] = useState("Contact Us");
+  const form = useRef();
+  const status = "Send"
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6fwg831', 'contact_form', form.current, init("user_0VcUZrsMJ5pqKELU1Q6Vm"))
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    
+  };
+
+  // const [status, setStatus] = useState("Contact Us");
 
   const applyStyling = (e) => {
     if(!e.target.classList.contains("input-placeholder")) {
@@ -9,51 +25,51 @@ const Form = () => {
     }
   }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
 
-    setStatus("Sending...");
+  //   setStatus("Sending...");
 
     
-    const [ name, email, subject, message ] = e.target.elements;
+  //   const [ name, email, subject, message ] = e.target.elements;
     
-    let details = {
-      name: name.value,
-      email: email.value,
-      subject: subject.value,
-      message: message.value,
-    };
+  //   let details = {
+  //     name: name.value,
+  //     email: email.value,
+  //     subject: subject.value,
+  //     message: message.value,
+  //   };
 
-    // "proxy": "https://wabisabi-mailer.herokuapp.com/"
-      let response = await fetch("loca/contact", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json;charset=utf-8",
-        },
-        body: JSON.stringify(details),
-      });
+  //   // "proxy": "https://wabisabi-mailer.herokuapp.com/"
+  //     let response = await fetch("loca/contact", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json;charset=utf-8",
+  //       },
+  //       body: JSON.stringify(details),
+  //     });
   
-      setStatus("Submit");
+  //     setStatus("Submit");
   
-      let result = await response.json();
+  //     let result = await response.json();
       
-      alert(result.status);
+  //     alert(result.status);
   
-      reset(e)
-  };
+  //     reset(e)
+  // };
 
-  const reset = (e) => {
-    const inputs = document.querySelectorAll("input-placeholder");
-    inputs.forEach(input => input.classList.remove("input-placeholder"))
-    e.target.reset(e);
-  }
+  // const reset = (e) => {
+  //   const inputs = document.querySelectorAll("input-placeholder");
+  //   inputs.forEach(input => input.classList.remove("input-placeholder"))
+  //   e.target.reset(e);
+  // }
 
   return (
-    <form id="form" className="contact-form" onSubmit={handleSubmit}>
-        <input onClick={applyStyling} className="contact-input" placeholder="Name" required></input>
-        <input onClick={applyStyling} className="contact-input" placeholder="Email" required></input>
-        <input onClick={applyStyling} className="contact-input" placeholder="Subject" required></input>
-        <textarea rows="4" onClick={applyStyling} className="contact-input" placeholder="Message" required></textarea>
+    <form ref={form} id="form" className="contact-form" onSubmit={sendEmail}>
+        <input onClick={applyStyling} className="contact-input" placeholder="Name" name="user_name" required></input>
+        <input onClick={applyStyling} className="contact-input" placeholder="Email" name="reply_to" required></input>
+        <input onClick={applyStyling} className="contact-input" placeholder="Subject" name="subject" required></input>
+        <textarea rows="4" onClick={applyStyling} className="contact-input" placeholder="Message" name="message" required></textarea>
         <button type="submit" className="contact-btn animation">{status}</button>
     </form>
   );
